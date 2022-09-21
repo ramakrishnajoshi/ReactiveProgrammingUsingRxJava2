ae359a9b7a8947cf8876e1cd079cc2ca7f5d7d61
Ramakrishna Joshi <rjoshi@falabella.cl>
2022-09-21 14:21:43 +0530

    Adde flatmap

---
+++ app/src/main/java/com/example/reactiveprogrammingusingrxjava2/e_flatmap/User.java
@@ -0,0 +1,41 @@
+package com.example.reactiveprogrammingusingrxjava2.e_flatmap;
+
+public class User {
+    String name;
+    String email;
+    String gender;
+    Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+// getters and setters
+}
